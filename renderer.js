0ac5472d5691ebd109823065cad28bbde01c8d4e
kzhrk <info@kzhrk.com>
2016-08-02 17:04:02 +0900

    added vue and style

--- renderer.js
+++ renderer.js
@@ -1,5 +1,5 @@
 const $ = require('jquery');
-const Scraping = require('./Scraping.js');
+const Scraping = require('./lib/Scraping.js');
 const download = require('./lib/download.js');
 const json2csv = require('json2csv');
 const fs       = require('fs');
@@ -129,13 +129,6 @@ $submit.on('click', ()=>{
 
 	    let dir = moment().format('YYYYMMDD-HHmmssSSSS');
 
-<<<<<<< 9ca8906d08b44f2b6399bebf0c2382bc1eacabd7
-    let dir = moment().format('YYYYMMDD-HHmmss');
-		let csv = json2csv({
-			data: result,
-			fields: fields
-		});
-=======
 			Promise.all(promises).then((values)=>{
 				let result = [];
 				let fields = [];
@@ -157,7 +150,6 @@ $submit.on('click', ()=>{
 						}
 					});
 				});
->>>>>>> fixed windows bug
 
 				let csv = json2csv({
 					data: result,
@@ -172,80 +164,4 @@ $submit.on('click', ()=>{
 			});
 		});
 	});
-
-	console.log(store);
 });
-
-// $submit.on('click', ()=>{
-// 	let scraping = new Scraping( $url.val() );
-
-// 	let groupProp = [];
-// 	let promises  = [];
-
-// 	$('.js-group').each((i, el)=>{
-// 		let type = $(el).find('.js-type').val();
-
-// 		switch (type) {
-// 			case 'text':
-// 				groupProp.push({
-// 					name: $(el).find('.js-name').val(),
-// 					type: 'text'
-// 				});
-				
-// 				promises.push( scraping.getText( $(el).find('.js-selector').val() ) );
-// 				break;
-// 			case 'link':
-// 				groupProp.push({
-// 					name: $(el).find('.js-name').val(),
-// 					type: 'link'
-// 				});
-				
-// 				promises.push( scraping.getAttr( $(el).find('.js-selector').val(), 'href' ) );
-// 				break;
-// 			case 'img':
-// 				groupProp.push({
-// 					name: $(el).find('.js-name').val(),
-// 					type: 'img'
-// 				});
-
-// 				promises.push( scraping.getAttr( $(el).find('.js-selector').val(), 'src' ) );
-// 				break;
-// 		}
-// 	});
-
-// 	Promise.all(promises).then((values)=>{
-// 		let result = [];
-// 		let fields = [];
-
-// 		groupProp.forEach((prop, typeIndex)=>{
-// 			if (prop.type === 'img') {
-// 				download( values[typeIndex] );
-// 			}
-// 		});
-
-// 		// create object
-// 		values.forEach((value, valueIndex)=>{
-// 			value.forEach((str, strIndex)=>{
-// 				result[strIndex] = result[strIndex] || {};
-// 				result[strIndex][groupProp[valueIndex].name] = str;
-
-// 				if (fields.indexOf(groupProp[valueIndex].name) === -1) {
-// 					fields.push(groupProp[valueIndex].name);
-// 				}
-// 			});
-// 		});
-
-//     let dir = moment().format('YYYYMMDD-HHmmss');
-// 		let csv = json2csv({
-// 			data: result,
-// 			fields: fields
-// 		});
-
-//     if (!fs.existsSync(dir)) fs.mkdirSync(dir);
-
-//     csv = iconv.encode(csv, 'shift-jis');
-
-// 		fs.writeFile(dir + '/file.csv', csv);
-// 	});
-// });
-
